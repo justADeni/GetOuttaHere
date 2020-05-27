ef57f73d43c8dcdf5f8f914ab115446cb50ad87e
Deni <59316835+prosteDeniGC@users.noreply.github.com>
2020-05-28 00:56:06 +0200

    Update GetOuttaHere.java
    first version that actually works

--- GetOuttaHere.java
+++ GetOuttaHere.java
@@ -1,28 +1,22 @@
 package me.prostedeni.goodcraft.getouttahere;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class GetOuttaHere extends JavaPlugin implements Listener {
 
     public ArrayList<String> plist = new ArrayList<>();
-
-    private static int getRandomNumberInRange(int min, int max) {
-
-        if (min >= max) {
-            throw new IllegalArgumentException("max must be greater than min");
-        }
-
-        Random r = new Random();
-        return r.nextInt((max - min) + 1) + min;
-    }
 
     @EventHandler
     public void onPlayerLoginEvent(PlayerLoginEvent e){
@@ -30,17 +24,43 @@ public final class GetOuttaHere extends JavaPlugin implements Listener {
         if (e.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
             // If the condition above is true, we execute the following code, that is allow the player on the server
             if (e.getPlayer().hasPermission("getouttahere.stay")) {
-                for(Player p : Bukkit.getOnlinePlayers()){
-                    if (!(p.hasPermission("getouttahere.stay"))){
-                        plist.add(p.getName());
-                    }
+                if (plist.size() > 0) {
+                            int randomNumber = ThreadLocalRandom.current().nextInt(0, plist.size());
+                            String theChosenOne = plist.get(randomNumber);
+                            Player p = Bukkit.getServer().getPlayer(theChosenOne);
+                            String mess1 = getConfig().getString("KickMessage");
+                            String msgcolor1 = (ChatColor.translateAlternateColorCodes('&',mess1));
+                            p.kickPlayer(msgcolor1);
+                            e.allow();
+            } else if (plist.size() == 0){
+                            String mess2 = getConfig().getString("NoKickMessage");
+                            String msgcolor2 = (ChatColor.translateAlternateColorCodes('&',mess2));
+                            e.disallow(PlayerLoginEvent.Result.KICK_OTHER, msgcolor2);
                 }
-                int randomNumber = getRandomNumberInRange(1,plist.size());
-                String theChosenOne = plist.get(randomNumber);
-                Player p = Bukkit.getServer().getPlayer(theChosenOne);
-                p.kickPlayer(getConfig().getString("Message:"));
-                e.allow();
             }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        if (!(e.getPlayer().hasPermission("getouttahere.stay"))){
+            if (!(plist.contains(e.getPlayer().getName()))) {
+                plist.add(e.getPlayer().getName());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e){
+        if (plist.contains(e.getPlayer().getName())){
+            plist.remove(e.getPlayer().getName());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerKicked(PlayerKickEvent e){
+        if (plist.contains(e.getPlayer().getName())){
+            plist.remove(e.getPlayer().getName());
         }
     }
 
@@ -50,6 +70,12 @@ public final class GetOuttaHere extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         saveDefaultConfig();
+
+        for(Player p : Bukkit.getOnlinePlayers()){
+            if (!(p.hasPermission("getouttahere.stay"))){
+                plist.add(p.getName());
+            }
+        }
     }
 
     @Override
